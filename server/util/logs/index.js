df236941995778c2a5d92b6326d3564b0b7ce8cf
zanjs <root@zanjs.com>
2016-02-01 14:36:37 +0800

    :man: - Mon Feb 01 2016 14:36:37 GMT+0800 (中国标准时间) from gitpush

--- server/util/logs/index.js
+++ server/util/logs/index.js
@@ -5,7 +5,7 @@ const bunyan = require('bunyan');
 const config = require('../../config/env');
 
 const logger = bunyan.createLogger({
-	name: 'jackblog',
+	name: 'julian',
 	serializers: {
 	 req: bunyan.stdSerializers.req,
 	 res: bunyan.stdSerializers.res,
